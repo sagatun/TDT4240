f04a3b89d15e8f0ef3c4028e1d8c089d7ccc4b65
sagatun <eriksaga@stud.ntnu.no>
2021-02-01 19:51:26 +0100

    init local commit

---
+++ exercise_1/helicopter_demo/desktop/src/it/sagatun/helicopter_demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package it.sagatun.helicopter_demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import it.sagatun.helicopter_demo.HelicopterDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelicopterDemo(), config);
+	}
+}
